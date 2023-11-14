b5865e0f1123ac202d504c9aae5512681b6cdaa3
rangga <ranggantwleng77@gmail.com>
2023-11-14 17:01:02 +0700

    jetcoffee-Kotlin

---
+++ app/src/main/java/com/rangga/jetcoffee/ui/components/HomeSection.kt
@@ -0,0 +1,16 @@
+import androidx.compose.foundation.layout.Column
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import com.rangga.jetcoffee.SectionText
+
+@Composable
+fun HomeSection(
+    title: String,
+    modifier: Modifier = Modifier,
+    content: @Composable () -> Unit
+) {
+    Column(modifier) {
+        SectionText(title, modifier)
+        content()
+    }
+}
